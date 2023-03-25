b88cfecfba248a488f8f69153b082dbff6c36004
Orvin <orvin.tritama@hotmail.com>
2023-03-24 22:24:13 -0700

    redirect user if trying to register with existing username/pass

--- includes/handler/register-handler.php
+++ includes/handler/register-handler.php
@@ -8,35 +8,45 @@
         $email = $_POST['email'];
 
         $hashPassword = md5($password);
-
-        if($_FILES['profile-picture']['name'] !== "") {
-            // echo "reached here";
-            $imageData = file_get_contents($_FILES['profile-picture']['tmp_name']);
-            // echo "reached here";
-            $imageName = $_FILES['profile-picture']['name'];
-            $insertQuery = "INSERT INTO user(fullName, username, password, email, imageName, imageData) 
-            VALUES(?, ?, ?, ?, ?, ?)";
-
-            $stmt = $con->prepare($insertQuery);
-            $stmt->bind_param("sssssb", $fullname, $username, $hashPassword, $email, $imageName, $imageData);
-
-        } else {
-
-            $insertQuery = "INSERT INTO user(fullName, username, password, email) 
-            VALUES(?, ?, ?, ?)";
-
-            $stmt = $con->prepare($insertQuery);
-            $stmt->bind_param("ssss", $fullname, $username, $hashPassword, $email);
-
-        } 
-
         
-
-        if($stmt->execute()) {
-            // echo "Created new user";
-            header("Location: http://localhost/project-orvintritama/login.php");
+        // check if username or email exists
+        $checkExistQuery = mysqli_query($con,"SELECT COUNT(*) as count FROM user WHERE username='$username' OR email='$email'");
+        $result = mysqli_fetch_array($checkExistQuery);
+        $count = $result['count'];
+        if($count > 0) {
+            echo "Username or e-mail already exists. Please create with another username/email";
+            echo "<a href='http://localhost/project-orvintritama/register.php'> Click here to redirect </a>";
         } else {
-            echo "Fail to create new user";
-        };
+
+            if($_FILES['profile-picture']['name'] !== "") {
+                // echo "reached here";
+                $imageData = file_get_contents($_FILES['profile-picture']['tmp_name']);
+                // echo "reached here";
+                $imageName = $_FILES['profile-picture']['name'];
+                $insertQuery = "INSERT INTO user(fullName, username, password, email, imageName, imageData) 
+                VALUES(?, ?, ?, ?, ?, ?)";
+
+                $stmt = $con->prepare($insertQuery);
+                $stmt->bind_param("sssssb", $fullname, $username, $hashPassword, $email, $imageName, $imageData);
+
+            } else {
+
+                $insertQuery = "INSERT INTO user(fullName, username, password, email) 
+                VALUES(?, ?, ?, ?)";
+
+                $stmt = $con->prepare($insertQuery);
+                $stmt->bind_param("ssss", $fullname, $username, $hashPassword, $email);
+
+            } 
+
+            
+
+            if($stmt->execute()) {
+                // echo "Created new user";
+                header("Location: http://localhost/project-orvintritama/login.php");
+            } else {
+                echo "Fail to create new user";
+            };
+        }
     }
 ?>
